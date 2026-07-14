5f20ee914a71e9249c6d505e00d0f5cca40981b1
hacxy <hacxy.js@outlook.com>
2026-07-15 00:51:01 +0800

    fix(cli): 添加new子命令支持

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,16 @@ function bootstrap() {
       createProject(projectName, options);
     });
 
+  cli.command('new [template] [projectName]', '创建新项目')
+    .option('-t, --template <template>', '指定模板名称')
+    .option('--refresh', '强制刷新缓存')
+    .action((template: string | undefined, projectName: string | undefined, options: any) => {
+      if (template && !options.template) {
+        options.template = template;
+      }
+      createProject(projectName, options);
+    });
+
   cli.version(pkg.version);
   cli.help();
   cli.parse(process.argv);
